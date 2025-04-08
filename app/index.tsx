8e2de1adee773081f9615dd671a1161186db08a8
Omanshuaman <omanshuaman@gmail.com>
2025-04-08 13:31:08 +0530

    Implement Gluestack UI Provider with theming support
    - Added configuration for light and dark themes in config.ts
    - Created GluestackUIProvider component for managing theme and context
    - Implemented web-specific logic for theme handling in index.web.tsx
    - Added script for dynamic theme switching based on user preference
    - Defined ModeType for theme modes (light, dark, system)
    - Set up Tailwind CSS integration with global styles and configuration
    - Configured Metro bundler for NativeWind support
    - Established color variables for theming in Tailwind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 export default function HomeScreen() {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-    />
+    // <FlatList
+    //   data={products}
+    //   renderItem={({ item }) => <ProductListItem product={item} />}
+    // />
+    <Button>
+      <ButtonText>Click Me</ButtonText>
+    </Button>
   );
 }
