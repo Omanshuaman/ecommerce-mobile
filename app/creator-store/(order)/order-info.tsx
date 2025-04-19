3c4a8b1bdc1f88844925696db7b12e8c379c722c
Omanshuaman <omanshuaman@gmail.com>
2025-04-19 12:34:11 +0530

    feat: enhance OrderCard component with improved layout and functionality; add packing feature and delivery slip printing

--- app/creator-store/(order)/order-info.tsx
+++ app/creator-store/(order)/order-info.tsx
@@ -1,89 +1,108 @@
 import React, { useState } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 
 export default function OrderCard() {
   const [isPacked, setIsPacked] = useState(false);
 
   return (
-    <View className="p-4 bg-[#161616] h-full">
-      <View className="border border-gray-100 rounded-sm mb-2 bg-black">
-        <View className="flex-col justify-between items-start p-2">
-          <View className="flex-row items-center gap-3">
+    <View className="p-4 bg-[#161616] h-full flex-1">
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View className="border border-gray-100 rounded-md bg-black py-1">
+          <View className="flex-col justify-between items-start px-4 py-3">
+            <View className="flex-row items-center gap-3">
+              <Image
+                source={{
+                  uri: "https://randomuser.me/api/portraits/men/1.jpg",
+                }}
+                className="w-7 h-7 rounded-full"
+              />
+              <View>
+                <Text className="text-white font-bold text-lg">
+                  Samuil Sadovsky
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View className="border-t border-dashed border-white mb-2" />
+          <View className="flex-row items-center px-4 py-2">
             <Image
               source={{
-                uri: "https://randomuser.me/api/portraits/women/1.jpg",
+                uri: "https://randomuser.me/api/portraits/men/6.jpg",
               }}
-              className="w-7 h-7 rounded-full"
+              className="size-16 rounded-sm mr-3"
             />
-            <View>
-              <Text className="text-white font-bold text-base">
-                Samuil Sadovsky
+            <View className="flex-1 gap-1">
+              <Text
+                className="text-white text-lg"
+                style={{ fontFamily: "HelveticaNeue-Medium" }}>
+                VisionCapture X1
+              </Text>
+              <Text
+                className="text-white text-base"
+                style={{ fontFamily: "HelveticaNeue-Light" }}>
+                Like New
+              </Text>
+            </View>
+            <View className="items-end gap-1">
+              <Text
+                className="text-white text-lg"
+                style={{ fontFamily: "HelveticaNeue-Medium" }}>
+                $25.00
+              </Text>
+              <Text
+                className="text-white text-base"
+                style={{ fontFamily: "HelveticaNeue-Light" }}>
+                x16
               </Text>
             </View>
           </View>
-        </View>
-        <View className="border-t border-dashed border-white mb-2" />
-        <View className="flex-row items-center">
-          <Image
-            source={{
-              uri: "https://randomuser.me/api/portraits/men/6.jpg",
-            }}
-            className="h-14 w-14 rounded mr-3"
-          />
-          <View className="flex-1 mr-2">
-            <Text className="text-white font-bold text-sm">
-              VisionCapture X1
+          <TouchableOpacity
+            onPress={() => setIsPacked(true)}
+            disabled={isPacked}
+            className="border border-white rounded-md py-2 my-3 mx-4"
+            style={{
+              borderColor: isPacked ? "green" : "white",
+            }}>
+            <Text
+              className="text-center p-1"
+              style={{
+                fontFamily: "PPFormulaCondensed-Bold",
+                fontSize: 28,
+                color: isPacked ? "#A3FAC7" : "white",
+              }}>
+              {isPacked ? "PACKED" : "MARK AS PACKED"}
             </Text>
-            <Text className="text-white text-xs">Like New</Text>
-          </View>
-          <View className="items-end">
-            <Text className="text-white font-bold text-sm">$25.00</Text>
-            <Text className="text-white text-xs">x16</Text>
-          </View>
+          </TouchableOpacity>
         </View>
+
         <TouchableOpacity
-          onPress={() => setIsPacked(true)}
-          disabled={isPacked}
+          disabled={!isPacked}
+          className="border border-white rounded-sm py-2 my-3"
           style={{
-            borderWidth: 1,
-            borderRadius: 8,
-            paddingVertical: 8,
             borderColor: isPacked ? "green" : "white",
-            backgroundColor: isPacked ? "green" : "transparent",
           }}>
-          <Text className="text-center font-bold text-white">
-            {isPacked ? "PACKED" : "MARK AS PACKED"}
+          <Text
+            style={{
+              color: isPacked ? "#A3FAC7" : "white",
+              fontFamily: "PPFormulaCondensed-Bold",
+              fontSize: 28,
+            }}
+            className="text-center p-1 uppercase">
+            {isPacked ? "Delivery slip printed" : "PRINT DELIVERY SLIP"}
           </Text>
         </TouchableOpacity>
-      </View>
+      </ScrollView>
 
       <TouchableOpacity
         disabled={!isPacked}
-        style={{
-          backgroundColor: isPacked ? "white" : "#2d2d2d",
-        }}
-        className="rounded p-3 my-2">
+        className="bg-[#E5FF03] rounded-sm w-full py-3 justify-center items-center shadow-lg shadow-slate-50">
         <Text
           style={{
-            color: isPacked ? "black" : "#7d7d7d",
+            fontFamily: "PPFormulaCondensed-Bold",
+            fontSize: 30,
           }}
-          className="text-center font-bold">
-          PRINT DELIVERY SLIP
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        disabled={!isPacked}
-        style={{
-          backgroundColor: isPacked ? "#facc15" : "#854d0e",
-        }}
-        className="rounded p-3 mt-8">
-        <Text
-          style={{
-            color: isPacked ? "black" : "#d97706",
-          }}
-          className="text-center font-bold">
-          MARK FOR DELIVERY
+          className="text-center text-black uppercase">
+          Mark for delivery
         </Text>
       </TouchableOpacity>
     </View>
